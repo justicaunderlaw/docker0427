b827f87e654df684af91a517a86d6d950607cd61
justicaunderlaw <70931652+justicaunderlaw@users.noreply.github.com>
2022-04-27 17:17:56 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "e0ccdead-f66c-4cc5-95e2-af71f1a9ab71",
                         "alterId": 0
                     }
                 ],
